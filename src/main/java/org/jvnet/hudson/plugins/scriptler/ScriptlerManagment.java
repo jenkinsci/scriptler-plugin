5b89588410082c81ab5642ed9ad8b5361abad824
domi <domi@dev.java.net>
2010-07-18 18:07:44 +0000

    add javadoc

--- src/main/java/org/jvnet/hudson/plugins/scriptler/ScriptlerManagment.java
+++ src/main/java/org/jvnet/hudson/plugins/scriptler/ScriptlerManagment.java
@@ -24,7 +24,6 @@
 package org.jvnet.hudson.plugins.scriptler;
 
 import hudson.Extension;
-import hudson.ProxyConfiguration;
 import hudson.Util;
 import hudson.model.Computer;
 import hudson.model.ComputerSet;
@@ -34,15 +33,11 @@ import hudson.security.Permission;
 import hudson.util.RemotingDiagnostics;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -68,6 +63,9 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 /**
+ * Creates the link on the "manage hudson" page and handles all the web
+ * requests.
+ * 
  * @author domi
  * 
  */
@@ -128,8 +126,23 @@ public class ScriptlerManagment extends ManagementLink {
 		return cfg;
 	}
 
+	/**
+	 * Downloads a script from a catalog and imports it to the local system.
+	 * 
+	 * @param res
+	 *            request
+	 * @param rsp
+	 *            response
+	 * @param name
+	 *            the name of the file to be downloaded
+	 * @param catalogName
+	 *            the catalog to download the file from
+	 * @return same forward as from <code>doScriptAdd</code>
+	 * @throws IOException
+	 */
 	public HttpResponse doDownloadScript(StaplerRequest res, StaplerResponse rsp, @QueryParameter("name") String name,
 			@QueryParameter("catalog") String catalogName) throws IOException {
+		checkPermission(Hudson.ADMINISTER);
 
 		CatalogInfo catInfo = getConfiguration().getCatalogInfo(catalogName);
 		CatalogManager catalogManager = new CatalogManager(catInfo);
@@ -140,8 +153,25 @@ public class ScriptlerManagment extends ManagementLink {
 		return doScriptAdd(res, rsp, name, entry.comment, source);
 	}
 
+	/**
+	 * Saves a script snipplet as file to the system.
+	 * 
+	 * @param res
+	 *            response
+	 * @param rsp
+	 *            request
+	 * @param name
+	 *            the name for the file
+	 * @param comment
+	 *            a comment
+	 * @param script
+	 *            script code
+	 * @return forward to 'index'
+	 * @throws IOException
+	 */
 	public HttpResponse doScriptAdd(StaplerRequest res, StaplerResponse rsp, @QueryParameter("name") String name, @QueryParameter("comment") String comment,
 			@QueryParameter("script") String script) throws IOException {
+		checkPermission(Hudson.ADMINISTER);
 
 		if (StringUtils.isEmpty(script) || StringUtils.isEmpty(name)) {
 			throw new IllegalArgumentException("name and script must not be empty");
@@ -163,7 +193,20 @@ public class ScriptlerManagment extends ManagementLink {
 		return new HttpRedirect("index");
 	}
 
+	/**
+	 * Removes a script from the config and filesystem.
+	 * 
+	 * @param res
+	 *            response
+	 * @param rsp
+	 *            request
+	 * @param name
+	 *            the name of the file to be removed
+	 * @return forward to 'index'
+	 * @throws IOException
+	 */
 	public HttpResponse doRemoveScript(StaplerRequest res, StaplerResponse rsp, @QueryParameter("name") String name) throws IOException {
+		checkPermission(Hudson.ADMINISTER);
 
 		// remove the file
 		File oldScript = new File(getScriptDirectory(), name);
@@ -178,11 +221,18 @@ public class ScriptlerManagment extends ManagementLink {
 	}
 
 	/**
-	 * Uploads a script to the defined directory
+	 * Uploads a script and stores it with the given filename to the
+	 * configuration. It will be stored on the filessytem.
+	 * 
+	 * @param req
+	 *            request
+	 * @return forward to index page.
+	 * @throws IOException
+	 * @throws ServletException
 	 */
 	public HttpResponse doUploadScript(StaplerRequest req) throws IOException, ServletException {
+		checkPermission(Hudson.ADMINISTER);
 		try {
-			Hudson.getInstance().checkPermission(Hudson.ADMINISTER);
 			File rootDir = getScriptDirectory();
 
 			ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
@@ -212,6 +262,15 @@ public class ScriptlerManagment extends ManagementLink {
 		}
 	}
 
+	/**
+	 * Loads the script information.
+	 * 
+	 * @param scriptName
+	 *            the name of the script
+	 * @param withSrc
+	 *            should the script sources be loaded too?
+	 * @return the script
+	 */
 	protected Script getScript(String scriptName, boolean withSrc) {
 		Script s = getConfiguration().getScriptByName(scriptName);
 		File scriptSrc = new File(getScriptDirectory(), scriptName);
@@ -228,15 +287,22 @@ public class ScriptlerManagment extends ManagementLink {
 	}
 
 	/**
-	 * display the screen to trigger a script
+	 * Display the screen to trigger a script. The source of the script get
+	 * loaded from the filesystem and placed in the request to display it on the
+	 * page before execution.
 	 * 
 	 * @param req
+	 *            request
 	 * @param rsp
+	 *            response
 	 * @param scriptName
+	 *            the name of the script to be executed
 	 * @throws IOException
 	 * @throws ServletException
 	 */
 	public void doRunScript(StaplerRequest req, StaplerResponse rsp, @QueryParameter("name") String scriptName) throws IOException, ServletException {
+		checkPermission(Hudson.ADMINISTER);
+
 		Script script = getScript(scriptName, true);
 		req.setAttribute("script", script);
 		// set default selection
@@ -245,21 +311,32 @@ public class ScriptlerManagment extends ManagementLink {
 	}
 
 	/**
-	 * trigger/run/execute the script on a slave and show the result/output
+	 * Trigger/run/execute the script on a slave and show the result/output. The
+	 * request then gets forward to <code>runscript.jelly</code> (This is
+	 * usually also where the request came from). The script passed to this
+	 * method gets restored in the request again (and not loaded from the
+	 * system). This way one is able to modify the script before execution and
+	 * reuse the modified version for further executions.
 	 * 
 	 * @param req
+	 *            request
 	 * @param rsp
+	 *            response
 	 * @param scriptName
+	 *            the name of the script
+	 * @param script
+	 *            the script code (groovy)
+	 * @param node
+	 *            the node, to execute the code on.
 	 * @throws IOException
 	 * @throws ServletException
 	 */
 	public void doTriggerScript(StaplerRequest req, StaplerResponse rsp, @QueryParameter("scriptName") String scriptName,
 			@QueryParameter("script") String script, @QueryParameter("node") String node) throws IOException, ServletException {
-
 		checkPermission(Hudson.ADMINISTER);
 
 		// set the script info back to the request, to display it together with
-		// the output
+		// the output.
 		Script tempScript = getScript(scriptName, false);
 		tempScript.setScript(script);
 		req.setAttribute("script", tempScript);
@@ -271,11 +348,13 @@ public class ScriptlerManagment extends ManagementLink {
 	}
 
 	/**
-	 * runs the execution on a given slave
+	 * Runs the execution on a given slave.
 	 * 
-	 * @param req
-	 * @param rsp
-	 * @param view
+	 * @param node
+	 *            where to run the script.
+	 * @param scriptTxt
+	 *            the script (groovy) to be executed.
+	 * @return the output
 	 * @throws IOException
 	 * @throws ServletException
 	 */
@@ -305,19 +384,42 @@ public class ScriptlerManagment extends ManagementLink {
 
 	}
 
+	/**
+	 * Loads the script by its name and forwards the request to "edit.jelly".
+	 * 
+	 * @param req
+	 *            request
+	 * @param rsp
+	 *            response
+	 * @param scriptName
+	 *            the name of the script to be loaded in to the edit view.
+	 * @throws IOException
+	 * @throws ServletException
+	 */
 	public void doEditScript(StaplerRequest req, StaplerResponse rsp, @QueryParameter("name") String scriptName) throws IOException, ServletException {
+		checkPermission(Hudson.ADMINISTER);
+
 		Script script = getScript(scriptName, true);
 		req.setAttribute("script", script);
 		req.getView(this, "edit.jelly").forward(req, rsp);
 	}
 
+	/**
+	 * Gets the names of all configured slaves, regardless whether they are
+	 * online.
+	 * 
+	 * @return list with all slave names
+	 */
+	@SuppressWarnings("deprecation")
 	public List<String> getSlaveNames() {
 		ComputerSet computers = Hudson.getInstance().getComputer();
 		List<String> slaveNames = computers.get_slaveNames();
 
+		// slaveNames is unmodifiable, therefore create a new list
 		List<String> test = new ArrayList<String>();
 		test.addAll(slaveNames);
 
+		// add 'magic' name for master, so all nodes can be handled the same way
 		if (!test.contains("(master)")) {
 			test.add("(master)");
 		}
