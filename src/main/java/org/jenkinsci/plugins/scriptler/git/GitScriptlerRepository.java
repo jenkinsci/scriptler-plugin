9b32467678149b53cfc95b7684100eaf39aef4e6
Basil Crow <me@basilcrow.com>
2024-01-09 13:49:20 -0800

    Forward compatibility with Guice 7

--- src/main/java/org/jenkinsci/plugins/scriptler/git/GitScriptlerRepository.java
+++ src/main/java/org/jenkinsci/plugins/scriptler/git/GitScriptlerRepository.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import jenkins.model.Jenkins;
 
