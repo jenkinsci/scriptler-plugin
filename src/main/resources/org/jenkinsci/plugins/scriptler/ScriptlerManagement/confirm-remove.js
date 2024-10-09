fb1b76b334dcf1bcd19634bb6ff831627c265f80
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-09 14:03:59 +0300

    [JENKINS-73885] Remove inline `onclick` handler from script removal form

---
+++ src/main/resources/org/jenkinsci/plugins/scriptler/ScriptlerManagement/confirm-remove.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const removeScriptButtons = document.querySelectorAll(".scriptler-remove-script-form");
+    removeScriptButtons.forEach(button => button.addEventListener("click", (e) => {
+        const name = e.currentTarget.getAttribute('data-name');
+        if (!confirm("Sure you want to delete [" + name + "]?")) {
+            e.preventDefault();
+        }
+    }));
+});
