e2d968707f108d794d12b97fcfc5ef9fcace92ca
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-18 11:57:34 -0700

    [JENKINS-73973] Improve CSP compatibility
    * extract inline event handlers from ScriptlerBuilder/config.jelly to
      scriptler.js
    * extract inline script block from ScriptlerBuilder/config.jelly to
      scriptler.js
    
    Co-authored-by: Basil Crow <me@basilcrow.com>

--- src/main/resources/org/jenkinsci/plugins/scriptler/ScriptlerManagement/confirm-remove.js
+++ src/main/resources/org/jenkinsci/plugins/scriptler/ScriptlerManagement/confirm-remove.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const removeScriptButtons = document.querySelectorAll(".scriptler-remove-script-form");
   removeScriptButtons.forEach((button) =>
     button.addEventListener("click", (e) => {
-      const name = e.currentTarget.getAttribute("data-name");
+      const name = e.currentTarget.dataset.name;
       if (!confirm("Sure you want to delete [" + name + "]?")) {
         e.preventDefault();
       }
