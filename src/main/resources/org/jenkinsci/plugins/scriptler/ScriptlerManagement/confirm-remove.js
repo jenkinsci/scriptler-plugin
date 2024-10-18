26f3fa5f3f5d4bbbdcea6285948c7d31c9027a9f
Basil Crow <me@basilcrow.com>
2024-10-18 11:44:05 -0700

    Format JavaScript files with Prettier

--- src/main/resources/org/jenkinsci/plugins/scriptler/ScriptlerManagement/confirm-remove.js
+++ src/main/resources/org/jenkinsci/plugins/scriptler/ScriptlerManagement/confirm-remove.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const removeScriptButtons = document.querySelectorAll(".scriptler-remove-script-form");
-    removeScriptButtons.forEach(button => button.addEventListener("click", (e) => {
-        const name = e.currentTarget.getAttribute('data-name');
-        if (!confirm("Sure you want to delete [" + name + "]?")) {
-            e.preventDefault();
-        }
-    }));
+document.addEventListener("DOMContentLoaded", () => {
+  const removeScriptButtons = document.querySelectorAll(".scriptler-remove-script-form");
+  removeScriptButtons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+      const name = e.currentTarget.getAttribute("data-name");
+      if (!confirm("Sure you want to delete [" + name + "]?")) {
+        e.preventDefault();
+      }
+    }),
+  );
 });
--- src/main/webapp/lib/scriptler.js
+++ src/main/webapp/lib/scriptler.js
@@ -1,45 +1,41 @@
-
-function scriptler_initDetailLink(rootURL, referenceTag){
-   var itemURL = referenceTag.getAttribute('data-item-url');
-   var selId = referenceTag.value;
-   var all = new Array();
-   all = document.getElementsByName('scriptlerScriptId');
-   for(var i = 0; i < all.length; i++) {
-	   if(referenceTag == all.item(i)){
-		   var detailsLinkTag = document.getElementsByName('showScriptlerDetailLink').item(i);
-		   if(selId.length != 0){
-			   detailsLinkTag .href=rootURL+"/" + itemURL + "scriptler/showScript?id=".concat(selId);
-			   detailsLinkTag .style.display = 'block';
-			}else{
-			   detailsLinkTag .style.display = 'none';
-			}
-	   }
-   }
+function scriptler_initDetailLink(rootURL, referenceTag) {
+  var itemURL = referenceTag.getAttribute("data-item-url");
+  var selId = referenceTag.value;
+  var all = new Array();
+  all = document.getElementsByName("scriptlerScriptId");
+  for (var i = 0; i < all.length; i++) {
+    if (referenceTag == all.item(i)) {
+      var detailsLinkTag = document.getElementsByName("showScriptlerDetailLink").item(i);
+      if (selId.length != 0) {
+        detailsLinkTag.href = rootURL + "/" + itemURL + "scriptler/showScript?id=".concat(selId);
+        detailsLinkTag.style.display = "block";
+      } else {
+        detailsLinkTag.style.display = "none";
+      }
+    }
+  }
 }
 
-
-function scriptler_descArguments(referenceTag, params){
-   var all = new Array();
-   all = document.getElementsByName('scriptlerScriptId');
-   for(var i = 0; i < all.length; i++) {
-	   if(referenceTag == all.item(i)){
-		   var desc = "";
-		   for(var j = 0; j < params.length; j++) {
-			   desc += j+": "+ params[j].name +" ";
-		   }
-		   var descriptionTag = document.getElementsByName('scriptlerParameters').item(i);
-		   descriptionTag.innerText = desc;
-	   }
-    }	   
+function scriptler_descArguments(referenceTag, params) {
+  var all = new Array();
+  all = document.getElementsByName("scriptlerScriptId");
+  for (var i = 0; i < all.length; i++) {
+    if (referenceTag == all.item(i)) {
+      var desc = "";
+      for (var j = 0; j < params.length; j++) {
+        desc += j + ": " + params[j].name + " ";
+      }
+      var descriptionTag = document.getElementsByName("scriptlerParameters").item(i);
+      descriptionTag.innerText = desc;
+    }
+  }
 }
 
-function scriptler_showParams(referenceTag, scriptId){
-	scriptlerBuilderDesc.getParameters(scriptId, function(t) {
-		var params = t.responseObject();
-		if(params != null){
-			scriptler_descArguments(referenceTag, params);
-		}
-    });
+function scriptler_showParams(referenceTag, scriptId) {
+  scriptlerBuilderDesc.getParameters(scriptId, function (t) {
+    var params = t.responseObject();
+    if (params != null) {
+      scriptler_descArguments(referenceTag, params);
+    }
+  });
 }
-
-
