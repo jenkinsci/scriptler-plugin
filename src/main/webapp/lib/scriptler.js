152edd95bcafa0cb7a5977b8212d98b4e88b95b1
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-18 11:57:34 -0700

    [JENKINS-73973] Improve CSP compatibility
    * extract inline event handlers from ScriptlerBuilder/config.jelly to
      scriptler.js
    * extract inline script block from ScriptlerBuilder/config.jelly to
      scriptler.js
    
    Co-authored-by: Basil Crow <me@basilcrow.com>

--- src/main/webapp/lib/scriptler.js
+++ src/main/webapp/lib/scriptler.js
@@ -1,5 +1,5 @@
-function scriptler_initDetailLink(rootURL, referenceTag) {
-  var itemURL = referenceTag.getAttribute("data-item-url");
+function scriptler_initDetailLink(rootUrl, referenceTag) {
+  var itemUrl = referenceTag.dataset.itemUrl;
   var selId = referenceTag.value;
   var all = new Array();
   all = document.getElementsByName("scriptlerScriptId");
@@ -7,7 +7,7 @@ function scriptler_initDetailLink(rootURL, referenceTag) {
     if (referenceTag == all.item(i)) {
       var detailsLinkTag = document.getElementsByName("showScriptlerDetailLink").item(i);
       if (selId.length != 0) {
-        detailsLinkTag.href = rootURL + "/" + itemURL + "scriptler/showScript?id=".concat(selId);
+        detailsLinkTag.href = rootUrl + "/" + itemUrl + "scriptler/showScript?id=".concat(selId);
         detailsLinkTag.style.display = "block";
       } else {
         detailsLinkTag.style.display = "none";
@@ -39,3 +39,42 @@ function scriptler_showParams(referenceTag, scriptId) {
     }
   });
 }
+
+Behaviour.specify("select[name='scriptlerScriptId']", "ScriptlerBuilderSelect", 0, function (element) {
+  const script = document.querySelector("#scriptler-builder-behaviour");
+  const rootUrl = script.dataset.rootUrl;
+  element.addEventListener("change", function (event) {
+    const target = event.target;
+    scriptler_initDetailLink(rootUrl, target);
+    scriptler_showParams(target, target.value);
+  });
+});
+
+Behaviour.specify("a[name='showScriptlerDetailLink']", "ScriptlerBuilderDetailLink", 0, function (element) {
+  element.addEventListener("click", function (event) {
+    event.preventDefault();
+    const target = event.target;
+    window.open(target.href, "window", "width=900,height=640,resizable,scrollbars,toolbar,menubar");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const script = document.querySelector("#scriptler-builder-behaviour");
+  const rootUrl = script.dataset.rootUrl;
+  const hasPermission = script.dataset.hasPermission;
+
+  var all = new Array();
+  all = document.getElementsByName("scriptlerScriptId");
+  for (var i = 0; i < all.length; i++) {
+    all.item(i).disabled = !hasPermission;
+    scriptler_initDetailLink(rootUrl, all.item(i));
+    scriptler_showParams(all.item(i), all.item(i).value);
+  }
+
+  // remember the job name to send it along with the form
+  var jobName = document.getElementsByName("name").item(0).value;
+  var allBackupJobNames = document.getElementsByName("backupJobName");
+  for (var i = 0; i < allBackupJobNames.length; i++) {
+    allBackupJobNames.item(i).value = jobName;
+  }
+});
